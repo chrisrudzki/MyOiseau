2c0b4fc8289795bfc7fe82e639ad25273a13c512
chrisrudzki <chrisrudzki7@gmail.com>
2025-11-18 14:22:56 -0800

    first commit with firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+import { getAuth } from "firebase/auth";
+
+
+const firebaseApp = initializeApp ({
+  apiKey: "AIzaSyCAUd7eHodqA4wO4MVZCjp3Y3I9OVDu_Mo",
+  authDomain: "cool-places-84e55.firebaseapp.com",
+  projectId: "cool-places-84e55",
+  storageBucket: "cool-places-84e55.firebasestorage.app",
+  messagingSenderId: "568081727341",
+  appId: "1:568081727341:web:f1fb2f4a13b53e008b2464"
+});
+
+const auth_in = getAuth(firebaseApp);
+
+export const auth = auth_in;
