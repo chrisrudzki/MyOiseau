40be31ebbc0e2f223f8f372f1b3323547157649a
chrisrudzki <chrisrudzki7@gmail.com>
2025-11-18 19:48:02 -0800

    added auth useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import StartScreen from "./components/StartScreen.jsx"
+import { auth } from "./firebase.js"
+
 
 function App() {
   const [curStartDisplay, setCurStartDisplay] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+         console.log(user, " User is logged in.");
+        setCurStartDisplay(false);
+      } else {
+        console.log("User is logged out.");
+        setCurStartDisplay(true);
+      }
+      //  setCurStartDisplay(false); // to take away start screen
+    });
+    return () => unsubscribe();
+  }, []);
   
   return (
     <>
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from "react";
-import { auth } from "../firebase.js";
+import { auth } from "../firebase";
 
 import {
   signInWithEmailAndPassword
